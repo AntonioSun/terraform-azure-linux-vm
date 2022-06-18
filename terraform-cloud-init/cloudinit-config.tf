2f6f1a90647c35c1db35111743ba637a0c56a24f
Antonio Sun <asun@ats.l5.ca>
2022-06-17 21:30:38 -0400

    - [+] add as-is

---
+++ terraform-cloud-init/cloudinit-config.tf
@@ -0,0 +1,10 @@
+data "template_cloudinit_config" "config" {
+  gzip          = true
+  base64_encode = true
+
+  # Main cloud-config configuration file.
+  part {
+    content_type = "text/cloud-config"
+    content      = "packages: ['httpie']"
+  }
+}
